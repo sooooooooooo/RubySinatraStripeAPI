afd18ee4c07018ba3db6a429a5792f6a09dcd9f5
sooooooooooo <zhigengqi@yahoo.com>
2021-02-03 21:09:54 -0600

    render template file at root route

--- server/server.rb
+++ server/server.rb
@@ -1,5 +1,19 @@
+require "stripe"
+require "dotenv"
 require "sinatra"
 
+Dotenv.load
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+
+# puts Stripe::Plan.list
+
+set :static, true
+# set :public_folder, "../client"
+set :public_folder, File.join(File.dirname(__FILE__), ENV["STATIC_DIR"])
+set :port, 4242
+
 get "/" do
-	"yeepee"
+	content_type "text/html"
+	send_file File.join(settings.public_folder, "index.html")
 end
+
