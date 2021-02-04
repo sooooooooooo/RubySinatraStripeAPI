5783b5604dc86d407b03de392a3e3076c7c534d3
sooooooooooo <zhigengqi@yahoo.com>
2021-02-03 21:43:38 -0600

    added webhook

--- server/server.rb
+++ server/server.rb
@@ -30,3 +30,43 @@ post "/my-post-route" do
 	# file API request to Stripe
 	data.to_json
 end
+
+
+
+# A webhook is an API that is a way for a third-party to tell YOU about something, rather than you, most of the time when we interact with an API we're creating or retrieving resources from the API, but a webhook is a way for a third-party to tell you when something happens.
+# So it's a very common tool that we use in all our integrations, and they are very dependable, so it's a great way to automate some of your workflow that based on these events that are firing.
+
+# For handling webhooks, this is one way that we can set up a webhook handler:
+# Post request is coming from Stripe to my service.
+# Using Sinatra
+post '/webhook' do
+  payload = request.body.read
+  event = nil
+
+  begin
+    event = Stripe::Event.construct_from(
+      JSON.parse(payload, symbolize_names: true)
+    )
+  rescue JSON::ParserError => e
+    # Invalid payload
+    status 400
+    return
+  end
+
+  # Handle the event
+  case event.type
+  when 'payment_intent.succeeded'
+    payment_intent = event.data.object # contains a Stripe::PaymentIntent
+    # Then define and call a method to handle the successful payment intent.
+    # handle_payment_intent_succeeded(payment_intent)
+  when 'payment_method.attached'
+    payment_method = event.data.object # contains a Stripe::PaymentMethod
+    # Then define and call a method to handle the successful attachment of a PaymentMethod.
+    # handle_payment_method_attached(payment_method)
+  # ... handle other event types
+  else
+    puts "Unhandled event type: #{event.type}"
+  end
+
+  status 200
+end
